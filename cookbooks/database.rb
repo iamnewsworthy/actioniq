80dc1e43d6b03622df7075cf3e869f79b6fc2b4a
iamnewsworthy <40440486+iamnewsworthy@users.noreply.github.com>
2018-06-20 18:33:08 -0400

    Update database.rb

--- cookbooks/database.rb
+++ cookbooks/database.rb
@@ -11,3 +11,31 @@ mysql_service 'default' do
   initial_root_password passwords['root_password']
   action [:create, :start]
 end
+
+# Create a prod database
+mysql_database 'prod' do
+  connection(
+    :host     => '127.0.0.1',
+    :username => 'root',
+    :password => node['prod']['mysql']['initial_root_password']
+  )
+  action :create
+end
+
+# Create a staging database
+mysql_database 'staging' do
+  connection(
+    :host     => '127.0.0.1',
+    :username => 'root',
+    :password => node['staging']['mysql']['initial_root_password']
+  )
+  action :create
+end
+
+# create prod user
+mysql_database_user 'prod_god' do
+  connection mysql_connection_info
+  password data_bag_item['password']  
+  privileges [:all]
+  action [:create, :grant]
+end
