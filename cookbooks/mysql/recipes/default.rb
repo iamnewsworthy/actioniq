4fe9962ce17c8196ca7198f8726c3c7774c544b7
iamnewsworthy <40440486+iamnewsworthy@users.noreply.github.com>
2018-06-20 19:14:12 -0400

    Update default.rb

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -1,1 +1,41 @@
-moving
+# Load MySQL passwords from the 'passwords' data bag.
+passwords = data_bag_item('passwords', 'mysql')
+
+# Configure the MySQL client.
+mysql_client 'default' do
+  action :create
+end
+
+# Configure the MySQL service.
+mysql_service 'default' do
+  initial_root_password passwords['root_password']
+  action [:create, :start]
+end
+
+# Create a prod database
+mysql_database 'prod' do
+  connection(
+    :host     => '127.0.0.1',
+    :username => 'root',
+    :password => node['prod']['mysql']['initial_root_password']
+  )
+  action :create
+end
+
+# Create a staging database
+mysql_database 'staging' do
+  connection(
+    :host     => '127.0.0.1',
+    :username => 'root',
+    :password => node['staging']['mysql']['initial_root_password']
+  )
+  action :create
+end
+
+# create prod user
+mysql_database_user 'prod_god' do
+  connection mysql_connection_info
+  password data_bag_item['admin_password']  
+  privileges [:all]
+  action [:create, :grant]
+end
