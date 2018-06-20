b61b96edd97a99584ee0498893f0880826aec796
iamnewsworthy <40440486+iamnewsworthy@users.noreply.github.com>
2018-06-20 19:34:36 -0400

    Update and rename default_monitors.rb to base_host_monitors.rb

--- cookbooks/nagios/recipes/base_host_monitors.rb
+++ cookbooks/nagios/recipes/base_host_monitors.rb
@@ -1,7 +1,7 @@
 # test the current system load average
 nagios_nrpecheck "check_load" do
   command "#{node['nagios']['plugin_dir']}/check_load"
-  warning_condition "6"
-  critical_condition "10"
+  warning_condition "8"
+  critical_condition "9"
   action :add
 end
