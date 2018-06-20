a7ba7268c95c699aafe9b15a6032fdfb9c10a531
iamnewsworthy <40440486+iamnewsworthy@users.noreply.github.com>
2018-06-20 19:23:36 -0400

    Update default.rb

--- cookbooks/nagios/recipes/default.rb
+++ cookbooks/nagios/recipes/default.rb
@@ -1,1 +1,13 @@
-okjkl
+#
+# Cookbook Name:: nagios
+# Recipe:: default
+#
+#install the software
+package 'nagios' do
+  action :install
+end
+
+#start the service
+service 'nagios' do
+  action [ :enable, :start ]
+end
