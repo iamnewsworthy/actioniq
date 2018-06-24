31dea1d2f044f5d63743b99a585604454dd2ba53
iamnewsworthy <40440486+iamnewsworthy@users.noreply.github.com>
2018-06-23 21:00:59 -0400

    Update monitoring.rb

--- roles/monitoring.rb
+++ roles/monitoring.rb
@@ -5,9 +5,28 @@ run_list(
   'recipe[nagios::server]'
 )
 
-default_attributes(
+default_attributes({
   :nagios => {
-    :server_auth_method => 'htauth',
-    :url => 'nagios.tools.example.com'
+    :server => {
+      ### START Install Verison and Method
+      :install_method => "package",
+      ### END Install Version and Method
+      :service_name => "nagios3",
+      :home => "/usr/lib/nagios3",
+      :conf_dir => "/etc/nagios3",
+      :config_dir => "/etc/nagios3/conf.d",
+      :cache_dir => "/var/cache/nagios3",
+      :state_dir => "/var/lib/nagios3",
+      :run_dir => "/var/run/nagios3",
+      :docroot => "/usr/share/nagios3/htdocs",
+      :server_name => "nagios",
+      :web_server => "nginx"
+    },
+    :client => {
+      :install_method => "package"
+    },
+    :server_auth_method => "htauth",
+    :url => "nagios.actioniq.com"
   }
-)
+})
+EOF
