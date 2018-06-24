94691adaecfcb972b6b5b21f40d4a4019197a021
iamnewsworthy <40440486+iamnewsworthy@users.noreply.github.com>
2018-06-23 21:11:28 -0400

    Update base_host_monitors.rb

--- cookbooks/nagios/recipes/base_host_monitors.rb
+++ cookbooks/nagios/recipes/base_host_monitors.rb
@@ -1,7 +1,16 @@
 # test the current system load average
 nagios_nrpecheck "check_load" do
   command "#{node['nagios']['plugin_dir']}/check_load"
-  warning_condition "8"
-  critical_condition "9"
+  warning_condition "6"
+  critical_condition "10"
+  action :add
+end
+
+# Check all non-NFS/tmp-fs disks.
+nagios_nrpecheck "check_all_disks" do
+  command "#{node['nagios']['plugin_dir']}/check_disk"
+  warning_condition "8%"
+  critical_condition "5%"
+  parameters "-A -x /dev/shm -X nfs -i /boot"
   action :add
 end
